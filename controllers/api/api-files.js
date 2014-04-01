e9b3ececfe5ccf4ad964d0a1505daab87f619d40
David Rey <david.rey.acosta@gmail.com>
2014-04-01 23:34:07 +0200

    Little refactor of files API

--- controllers/api/api-files.js
+++ controllers/api/api-files.js
@@ -2,65 +2,51 @@ exports.init = function(noderplate) {
   var files = {};
 
   files.upload = function(req, res) {
-    var file, room, options, checkFileExists,
-        analytics,clientIp, moveFile, saveToDb;
+    var roomFilesPath, mkdir, exists, rename, query, save, file, filepath;
 
-    file     = req.files;
-    room     = req.body.room;
-    clientIp = req.headers['x-forwarded-for'] ||
-               req.connection.remoteAddress ||
-               req.socket.remoteAddress ||
-               req.connection.socket.remoteAddress;
+    roomFilesPath = noderplate.config.roomFilesPath;
 
-    checkFileExists = req.core.data.checkFileExists;
-    moveFile        = req.core.data.moveFile;
-    saveToDb        = req.core.data.saveToDb;
+    mkdir  = req.core.filesystem.mkdir;
+    exists = req.core.filesystem.exists;
+    rename = req.core.filesystem.rename;
+    query  = req.core.data.query;
+    save   = req.core.data.save;
 
-    options = {
-      filename : file.files.name.replace(/\s/g, ''),
-      name     : file.files.name,
-      path     : '',
-      size     : file.files.size,
-      room     : room,
-      type     : file.files.type,
-      tmpPath  : file.files.path
+    file = {
+      filename : req.files.files.name.replace(/\s|%/g, '-'),
+      name     : req.files.files.name,
+      size     : req.files.files.size,
+      room     : req.body.room,
+      type     : req.files.files.type,
+      tmpPath  : req.files.files.path
     };
 
-    req.core.rooms.get({room: options.room})
-    .then(function(roomData) {
-      return req.core.data.createDirectory('./files/');
+    query('Room', 'findOne', {room: file.room})
+    .then(function(room) {
+      filepath = roomFilesPath;
+
+      return mkdir(filepath);
     })
     .then(function() {
-      return req.core.data.createDirectory('./files/' + options.room);
+      filepath = roomFilesPath + file.room;
+
+      return mkdir(filepath);
     })
     .then(function() {
-      options.path = './files/' + options.room + '/' + options.filename;
-      return checkFileExists(options.path);
+      filepath = roomFilesPath + file.room + '/' + file.filename;
+
+      return exists(filepath);
     })
     .then(function(exists) {
       if (exists) {
-        options.filename = Math.floor(Math.random() * 1000) + options.filename;
-        options.path     = './files/' + options.room + '/' + options.filename;
+        file.filename = Math.floor(Math.random() * 1000) + file.filename;
+        filepath      = roomFilesPath + file.room + '/' + file.filename;
       }
-      return moveFile(options.tmpPath, options.path);
+
+      return rename(file.tmpPath, filepath);
     })
     .then(function() {
-      analytics            = options;
-      analytics.clientData    = {
-        'accept-language': req.headers['accept-language'],
-        'accept-encoding': req.headers['accept-encoding'],
-        'referer'        : req.headers['referer'],
-        'user-agent'     : req.headers['user-agent'],
-        'origin'         : req.headers['origin'],
-        'accept'         : req.headers['accept'],
-        'content-length' : req.headers['content-length'],
-        'host'           : req.headers['host'],
-        'remote-ip'      : clientIp
-      };
-
-      saveToDb('Analytic', analytics);
-
-      return res.jsonp(saveToDb('File', options));
+      return res.jsonp(save('File', file));
     })
     .fail(function(err) {
       return res.jsonp(err);
