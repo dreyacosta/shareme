f2d4e1ecfd4cca1f78831987627d8c16d4320e34
David Rey <david.rey.acosta@gmail.com>
2014-04-02 23:28:33 +0200

    Refactor on Web Home controller

--- controllers/web/web-home.js
+++ controllers/web/web-home.js
@@ -2,31 +2,28 @@ exports.init = function(noderplate) {
   var home = {};
 
   home.render = function(req, res) {
-    if (req.params.room !== undefined) {
-      req.core.rooms.get({room: req.params.room})
-      .then(function(response) {
-        req.core.files.getFiles()
-        .then(function(files) {
-          return res.render('index', {
-            title      : 'Noderplate',
-            sharefiles : files,
-            env        : noderplate.app.env
-          });
-        });
-      })
-      .fail(function(err) {
-        return res.render('roomnotfound', {});
-      });
-    } else {
-      req.core.files.getFiles()
-      .then(function(files) {
-        return res.render('index', {
-          title      : 'Noderplate',
-          sharefiles : files,
-          env        : noderplate.app.env
-        });
+    var room, query;
+
+    room  = req.params.room;
+    query = req.core.data.query;
+
+    if (!room) {
+      return res.render('index', {
+        title: 'Noderplate',
+        sharefiles: 0,
+        env: noderplate.app.env
       });
     }
+
+    query('Room', 'findOne', {room: room}).then(function() {
+      return res.render('index', {
+        title: 'Noderplate',
+        sharefiles: 0,
+        env: noderplate.app.env
+      });
+    }).fail(function(err) {
+      return res.render('roomnotfound', {});
+    });
   };
 
   return home;
