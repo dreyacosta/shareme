ea8374bce6f6e31ad5cb6af3f0394aebdab6b87e
David Rey <david.rey.acosta@gmail.com>
2014-04-06 20:52:48 +0200

    Some changes on core Unit Tests

--- spec/server/core.spec.js
+++ spec/server/core.spec.js
@@ -1,53 +1,87 @@
 var expect     = require('chai').expect,
     noderplate = require('../../noderplate');
 
-describe('Core filesystem', function(){
-  var core = noderplate.app.core;
-  var mockPath = __dirname + '/testdir/';
+var core = noderplate.app.core;
 
-  it('should create directory', function(done) {
-    core.filesystem.mkdir(mockPath)
-    .then(function() {
-      done();
+describe('Core', function(){
+  describe("Filesystem", function() {
+    var mockPath = __dirname + '/testdir/';
+
+    it('should create directory', function(done) {
+      core.filesystem.mkdir(mockPath)
+      .then(function() {
+        done();
+      });
+    });
+
+    it('should exist directory', function(done) {
+      core.filesystem.exists(mockPath).then(function(exists) {
+        expect(exists).to.equal(true);
+        done();
+      }).fail(function(err) {
+        done(err);
+      });
+    });
+
+    it('should rename directory', function(done) {
+      var mockNewPath = __dirname + '/testdirMove/';
+
+      core.filesystem.rename(mockPath, mockNewPath).then(function() {
+        mockPath = mockNewPath;
+        done();
+      }).fail(function(err) {
+        done(err);
+      });
+    });
+
+    it('should remove directory', function(done) {
+      core.filesystem.rmdir(mockPath)
+      .then(function() {
+        done();
+      })
+      .fail(function(err) {
+        done(err);
+      });
+    });
+
+    it('should not exist directory', function(done) {
+      core.filesystem.exists(mockPath).then(function(exists) {
+        expect(exists).to.equal(false);
+        done();
+      }).fail(function(err) {
+        done(err);
+      });
     });
   });
 
-  it('should exist directory', function(done) {
-    core.filesystem.exists(mockPath).then(function(exists) {
-      expect(exists).to.equal(true);
-      done();
-    }).fail(function(err) {
-      done(err);
+  describe("Data", function() {
+    it('should save model to database', function() {
+      var res = core.data.save('File', {
+        filename: 'Hello World',
+        size: 23821,
+        room: 'Demo'
+      });
+
+      expect(res.filename).to.equal('Hello World');
+      expect(res.size).to.equal('23821');
     });
-  });
 
-  it('should rename directory', function(done) {
-    var mockNewPath = __dirname + '/testdirMove/';
-
-    core.filesystem.rename(mockPath, mockNewPath).then(function() {
-      mockPath = mockNewPath;
-      done();
-    }).fail(function(err) {
-      done(err);
+    it('should retrieve files from a room', function(done) {
+      core.data.query('File', 'find', {room: 'Demo'}).then(function(files) {
+        expect(files.length).to.equal(1);
+        done();
+      }).fail(function(err) {
+        done(err);
+      });
     });
-  });
 
-  it('should remove directory', function(done) {
-    core.filesystem.rmdir(mockPath)
-    .then(function() {
-      done();
-    })
-    .fail(function(err) {
-      done(err);
-    });
-  });
-
-  it('should not exist directory', function(done) {
-    core.filesystem.exists(mockPath).then(function(exists) {
-      expect(exists).to.equal(false);
-      done();
-    }).fail(function(err) {
-      done(err);
+    it('should remove files from a room', function(done) {
+      core.data.query('File', 'remove', {room: 'Demo'}).then(function(removeEls) {
+        expect(removeEls).to.equal(1);
+        done();
+      }).fail(function(err) {
+        done(err);
+      });
     });
   });
 });
