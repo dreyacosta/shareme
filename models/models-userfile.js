c905ce7aa4bb7c35d95951dc922e27e1b94cd48f
David Rey <david.rey.acosta@gmail.com>
2014-04-27 23:32:30 +0200

    New userfile model

---
+++ models/models-userfile.js
@@ -0,0 +1,28 @@
+exports.init = function(noderplate) {
+  var mongoose = noderplate.imports.mongoose;
+
+  var userFileSchema = mongoose.Schema({
+    clicks: {
+      type: Number,
+      default: 0
+    },
+    filename: String,
+    name: String,
+    public: {
+      type: Boolean,
+      default: false
+    },
+    username: String,
+    size: String,
+    room: String,
+    type: String,
+    registerDate: {
+      type: Date,
+      default: Date.now
+    }
+  });
+
+  var UserFile = mongoose.model('userfile', userFileSchema);
+
+  return UserFile;
+};
