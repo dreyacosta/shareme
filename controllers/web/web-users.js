114289df59a97668af5d40a685c66f403a5521c2
David Rey <david.rey.acosta@gmail.com>
2014-04-27 23:31:38 +0200

    Refactor on users controller

--- controllers/web/web-users.js
+++ controllers/web/web-users.js
@@ -9,33 +9,76 @@ exports.init = function(noderplate) {
     }
 
     if (method === 'post') {
+      if (req.body.pass !== req.body.confirmPass) {
+        return res.render('register', {
+          status: 'error',
+          message: 'Pass doesnt match'
+        });
+      }
+
+      if (req.body.pass.length < 6) {
+        return res.render('register', {
+          status: 'error',
+          message: 'Pass must have a minimum of 6 character'
+        });
+      }
+
       var options = {
         username : req.body.username,
         pass     : req.body.pass,
         email    : req.body.email
       };
 
-      req.core.users.register(options)
-      .then(function(user) {
-        var mailOptions = {
-          from    : 'shareme.io',
-          to      : user.email,
-          subject : 'Hello ✔',
-          html    : '<b>Hello world ✔</b>'
-        };
+      var query = {
+        username : req.body.username
+      };
 
-        return req.core.sendmail.send(mailOptions);
-      })
-      .then(function(ress) {
-        return res.redirect('/');
+      req.core.data.query('User', 'findOne', query)
+
+      .then(function(user) {
+        if (user) {
+          return res.render('register', {
+            status: 'error',
+            message: 'Message'
+          });
+        }
+
+        req.core.utils.generateSalt(options.pass)
+
+        .then(function(resp) {
+          options.pass = resp.hash;
+          options.salt = resp.salt;
+          return req.core.data.save('User', options);
+        })
+        .then(function() {
+          var mailsettings = {
+            from: 'shareme.io',
+            to: options.email,
+            bcc: noderplate.config.sendmail.bcc,
+            subject: 'Confirm registration',
+            html: '<h1>Hello World!</h1>'
+          };
+          return req.core.sendmail.send(mailsettings);
+        })
+        .then(function() {
+          return res.redirect('/');
+        })
+        .fail(function(err) {
+          return res.render('register', {
+            status: 'error',
+            message: err
+          });
+        });
       })
       .fail(function(err) {
-        return res.send('Fail sendmail');
+
       });
     }
   };
 
   users.login = function(req, res) {
+    var user;
+
     var method = req.route.method;
 
     if (method === 'get') {
@@ -48,20 +91,31 @@ exports.init = function(noderplate) {
         pass     : req.body.pass
       };
 
-      req.core.users.login(options)
-      .then(function(user) {
-        req.session.authenticated = true;
+      var query = {
+        username : req.body.username
+      };
 
-        req.session.user = {
-          username     : user.username,
-          email        : user.email,
-          registerDate : user.registerDate
-        };
+      req.core.data.query('User', 'findOne', query)
 
-        return res.redirect('/');
+      .then(function(data) {
+        user = data;
+
+        return req.core.utils.compareHash(options.pass, user.pass);
       })
-      .fail(function() {
-        return res.redirect('/');
+      .then(function() {
+        req.session.authenticated = true;
+        req.session.user = {
+          username: user.username,
+          email: user.email,
+          registerDate: user.registerDate
+        };
+        return res.redirect('/profile/' + req.session.user.username);
+      })
+      .fail(function(err) {
+        return res.render('login', {
+          status: 'error',
+          message: err
+        });
       });
     }
   };
@@ -70,7 +124,19 @@ exports.init = function(noderplate) {
     var method = req.route.method;
 
     if (method === 'get') {
-      return res.render('profile', {});
+      if (req.params.username === req.session.user.username) {
+        req.core.data.query('UserFile', 'find', {username: req.session.user.username})
+
+        .then(function(files) {
+          var data = {
+            session: req.session,
+            files: files
+          };
+          return res.render('profile', data);
+        });
+      } else {
+        return res.redirect('/');
+      }
     }
   };
 
