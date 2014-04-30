266534fc62d1a3309967cef975fb6689060d157d
David Rey <david.rey.acosta@gmail.com>
2014-04-30 23:42:19 +0200

    Added new templates and modules

--- browserify/js/app.js
+++ browserify/js/app.js
@@ -28,16 +28,20 @@ app.regions = {
 
 app.templates = {
   file        : require('./templates/file'),
-  upload      : require('./templates/upload'),
+  connectRoom : require('./templates/connectRoom'),
+  infoRoom    : require('./templates/infoRoom'),
+  profileFile : require('./templates/profileFile'),
   profileName : require('./templates/profileName'),
   menu        : require('./templates/menu')
 };
 
-app.socket  = app.imports.io.connect();
-app.config  = require('./config');
-app.utils   = require('./utils').init(app);
-app.models  = require('./models').init(app);
-app.views   = require('./views').init(app);
-app.routers = require('./routers').init(app);
+app.socket      = app.imports.io.connect();
+app.config      = require('./config');
+app.utils       = require('./utils').init(app);
+app.sockets     = require('./sockets').init(app);
+app.models      = require('./models').init(app);
+app.collections = require('./collections').init(app);
+app.views       = require('./views').init(app);
+app.routers     = require('./routers').init(app);
 
 module.exports = app;
