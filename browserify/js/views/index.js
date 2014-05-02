cec2d73ea3402b11a68671a74c420aa24eafa89f
David Rey <david.rey.acosta@gmail.com>
2014-05-02 23:06:14 +0200

    Added new Backbone views to views module

--- browserify/js/views/index.js
+++ browserify/js/views/index.js
@@ -1,8 +1,11 @@
 exports.init = function(app) {
   var views = {};
 
-  views.Upload      = require('./views-upload').init(app);
+  views.ConnectRoom = require('./views-connectRoom').init(app);
+  views.InfoRoom    = require('./views-infoRoom').init(app);
   views.File        = require('./views-file').init(app);
+  views.Files       = require('./views-files').init(app);
+  views.ProfileFile = require('./views-profilefile').init(app);
   views.Menu        = require('./views-menu').init(app);
 
   return views;
