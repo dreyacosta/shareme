3f631220ec7af7928b60e146dd16be0b2bf1a1f0
David Rey <david.rey.acosta@gmail.com>
2014-05-04 14:26:39 +0200

    New client sockets module

---
+++ browserify/js/sockets/index.js
@@ -0,0 +1,20 @@
+exports.init = function(app) {
+  app.socket.on('room', function(data) {
+    app.room.set(data);
+  });
+
+  app.socket.on('files', function(data) {
+    data.forEach(function(file) {
+      var item = app.files.find(function(item){
+        return item.get('_id') === file._id;
+      });
+
+      if (item) {
+        return;
+      }
+
+      var fileModel = new app.models.File(file);
+      app.files.add(fileModel);
+    });
+  });
+};
